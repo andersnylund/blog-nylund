88f8d994282a8107c25fc513bf79c0067a091fd4
Anders Nylund <anders@andersnylund.com>
2022-02-06 21:15:36 +0200

    Code block syntax highlightning

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
-import markdownStyles from './markdown-styles.module.css';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import markdownStyles from './postbody-styles.module.css';
 
 type Props = {
   content: string;
@@ -7,11 +10,35 @@ type Props = {
 
 export const PostBody: FC<Props> = ({ content }) => {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+    <div className="max-w-2xl mx-auto unreset">
+      <ReactMarkdown
+        className={markdownStyles.markdown}
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                className="rounded-md"
+                style={darcula}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code
+                className={`${className} py-0.5 px-1 font-mono rounded-md text-zinc-600 bg-zinc-100 border-2`}
+                {...props}
+              >
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };

7908ab09b21e06499a69193833b0ebe8a1dbedbb
Anders Nylund <anders@andersnylund.com>
2022-02-13 12:30:16 +0200

    CHange to styled-components

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -1,9 +1,20 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
 export const PostTitle: FC = ({ children }) => {
-  return (
-    <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
-      {children}
-    </h1>
-  );
+  return <H1>{children}</H1>;
 };
+
+const H1 = styled.h1`
+  font-size: 2rem;
+  line-height: 1;
+  font-weight: 700;
+  letter-spacing: -0.05em;
+  line-height: 1.25;
+  margin-bottom: 3rem;
+
+  @media (min-width: 48rem) {
+    font-size: 3rem;
+    line-height: 1;
+  }
+`;
